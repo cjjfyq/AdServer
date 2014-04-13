79a1a43e1c94f41df2d8b7515c2538d213d422ee
fyqjnu <fyqjnu@qq.com>
2014-04-14 00:05:33 +0800

    no message

---
+++ src/com/adserver/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.adserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Gordon on 2014/4/14.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+}
