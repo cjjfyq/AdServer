00a139631d760d07df873ef5bb4337116e1fb2b3
fyqjnu <fyqjnu@qq.com>
2014-04-14 16:55:19 +0800

    添加配置文件

--- src/com/adserver/controller/TestController.java
+++ src/com/adserver/controller/TestController.java
@@ -3,11 +3,20 @@ package com.adserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Gordon on 2014/4/14.
  */
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+
+    @RequestMapping("/showpage")
+    public String showPage(HttpServletRequest request) {
+        System.out.println("进入 showpage方法----");
+        return "test";
+    }
 
 }
