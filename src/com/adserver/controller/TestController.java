e86a0cec158b119f9cbcea91e2336fa54647d80f
fyqjnu <fyqjnu@qq.com>
2014-04-14 18:03:53 +0800

    测试spring集成

--- src/com/adserver/controller/TestController.java
+++ src/com/adserver/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.adserver.controller;
 
+import com.adserver.controller.com.adserver.service.ITestSpring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -12,10 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/test")
 public class TestController {
 
+    @Resource(name = "testSpring")
+    private ITestSpring springManager;
 
     @RequestMapping("/showpage")
     public String showPage(HttpServletRequest request) {
         System.out.println("进入 showpage方法----");
+
+        String ret = springManager.test();
+        System.out.println("ret from spring: " + ret);
+
         return "test";
     }
 
---
+++ src/com/adserver/controller/com/adserver/service/ITestSpring.java
@@ -0,0 +1,9 @@
+package com.adserver.controller.com.adserver.service;
+
+/**
+ * Created by Administrator on 2014/4/14.
+ */
+public interface ITestSpring {
+
+    String test();
+}
